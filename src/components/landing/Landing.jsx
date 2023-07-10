0a80be6ffe089e4acd99c52eaebc139ad35da8ec
sweeeeetpotato <sweet_potato13@naver.com>
2023-07-10 17:20:37 +0900

    modify : 랜딩페이지 로고 이미지파일 변경 및 코드 삭제

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "assets/logo.svg";
 import logoText from "assets/logo-text.svg";
 import logoImg from "assets/logo-img.svg";
 import styles from "./landing.module.css";
@@ -9,7 +8,6 @@ export default function Landing() {
     <section className={styles.container}>
       <div className={styles.wrapper}>
         <img src={logoText} alt="Five My Seat" className={styles.logo_text} />
-        <img src={logo} alt="Five My Seat" className={styles.logo} />
       </div>
       <img src={logoImg} alt="로고 이미지" className={styles.logo_img} />
     </section>
